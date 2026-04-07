48df489f699f58648ffe1f81140accd08cf6cc28
Mangondato9Say2526 <mangondato.ash@obmontessori.edu.ph>
2026-04-07 08:49:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const u = "ashmngndt";   
+const p = "mango1234"; 
+
+
+function login() {
+    let usernameInput = document.getElementById("UserName").value;
+    let passwordInput = document.getElementById("Password").value;
+
+    if (usernameInput === "ashmngndt" && passwordInput === mango1234) {
+        alert("Login successful!");
+    } 
+
+    else {
+        alert("Incorrect username or password.");
+    }
+}
